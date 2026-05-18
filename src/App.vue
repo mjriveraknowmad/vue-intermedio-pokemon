bb0faab0a55561fad8c62a38007094c281b0b720
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-18 09:05:30 +0200

    feat: Inicio de aplicación

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+import NavBar from '@/shared/components/NavBar.vue';
+import { routerLinks } from '@/router/list-routes';
+
+</script>
+
+<template>
+  Aplicación de Vue 3 Intermedio.
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+
+
+</style>
